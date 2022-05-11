529716e0843d450d6ae81291cc1e355504474bb5
sergiorios777 <sergio.rios777@gmail.com>
2022-05-11 10:57:16 -0500

    Actualización galeria.js

--- galeria/galeria.js
+++ galeria/galeria.js
@@ -9,12 +9,12 @@ imgs.forEach((el) =>
             const figGrande = document.querySelector('.imagen-grande figure');
             const figTitulo = document.querySelector('.imagen-grande figcaption');
 
-            divGrande.innerHTML = "";
+            //divGrande.innerHTML = "";
 
             imgGrande.setAttribute('src', el.src.replace('thumbs/', 'medios/'));
-            figGrande.appendChild(imgGrande);
+            //figGrande.appendChild(imgGrande);
             figTitulo.textContent = "titulo del imagen";
-            divGrande.appendChild(figGrande);
+            //divGrande.appendChild(figGrande);
 
             //imgGrande.addEventListener('click', mostrarImagen(el.src.replace('thumbs/', 'imagenes/')));
         })
